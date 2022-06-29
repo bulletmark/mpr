e1104b1a24fef1b4fa3bd2b4765b3391f3332940
Mark Blakeney <mark.blakeney@bullet-systems.net>
2022-06-30 08:37:27 +1000

    Version 1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = Path(__file__).resolve().parent
 
 setup(
     name=name,
-    version='1.1',
+    version='1.2',
     description='Wrapper for MicroPython mpremote tool',
     long_description=here.joinpath('README.md').read_text(),
     long_description_content_type='text/markdown',
